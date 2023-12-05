c55833f829cdf3e9c19d3bf78c6d22ad731aba52
pustulm <pustulm96@gmail.com>
2023-12-05 15:38:16 +0100

    terraform

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -52,7 +52,7 @@ resource "kubernetes_deployment" "timelog2" {
   }
 }
 
-resource "kubernetes_service_v1" "timelog2svc" {
+resource "kubernetes_service" "timelog2svc" {
   metadata {
     name = "timelog2svc"
     namespace = kubernetes_namespace.timelog2test.metadata.0.name
